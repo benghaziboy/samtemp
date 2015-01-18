96197114d51cd67b96bd82076c4075279df8902b
Cameron Lowe <cam.m.lowe@gmail.com>
2015-01-18 15:14:52 -0600

    Adds RenderTemplate function

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"io"
 )
 
-func main() {
-	fmt.Println("Let's template and send some emails.")
+// RenderTemplate renders the template file provided
+// Context is interpretted from the context []string argument
+// Returns the returns a string of the rendered template
+func RenderTemplate(html string, context []string) error {
+	temp, err := template.ParseFiles(html)
+	if err != nil {
+		return err
+	}
+
+	var out io.Writer
+	err = temp.Execute(out, context)
+
+	return err
 }
